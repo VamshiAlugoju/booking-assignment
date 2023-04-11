ffdaf10d49585bb663c974687f9ec4a0d5695293
vamshi <alugojuvamshi@gmail.com>
2023-04-11 12:25:35 +0530

    everything si good

--- index.js
+++ index.js
@@ -1,109 +1,119 @@
+let Userdata = [];
 
- let ulItems = document.querySelector("#items")
- let nameInp = document.querySelector("#name")
- let emailInp = document.querySelector("#email")
- let phoneInp = document.querySelector("#phone")
- window.addEventListener("load" , docall)
-    
-    function docall()
-    {
-        axios.get("https://crudcrud.com/api/5e23e7d240c14587b1b0194169f1a010/users")
-        .then(res=>{
-            // console.log(res.data)
-            // showOutput(res.data)
-            res.data.forEach(user=>{
-                // console.log(user.name)
-                localStorage.setItem(user.name , JSON.stringify(user))
-            })
-        })
-        .catch(err=>console.log(err))
+let ulItems = document.querySelector("#items");
+let nameInp = document.querySelector("#name");
+let emailInp = document.querySelector("#email");
+let phoneInp = document.querySelector("#phone");
+window.addEventListener("load", OnLoad);
 
-        showOutput();
-    }
+// function that gets called for the first time window loads
+function OnLoad() {
+  axios
+    .get("https://crudcrud.com/api/eb7de077b62e4abab2890bff988fc248/users")
+    .then((res) => {
+      LoadListItems(res.data);
+    })
+    .catch((err) => console.log( "error is ", err));
+}
 
 
-    function dothis(e)
-    {
-        e.preventDefault();
-        let user = e.target;
-         
-        let userobj = {
-            name:user.name.value,
-            email:user.email.value,
-            phone:user.phone.value,
-        }
-        localStorage.setItem(userobj.name,JSON.stringify(userobj));
-        axios
-        .post(
-          "https://crudcrud.com/api/5e23e7d240c14587b1b0194169f1a010/users",
-          userobj
-        )
-        .then((res) => {
-          console.log(res);
-          addtoDom(JSON.parse(localStorage.getItem(userobj.name)))
-        })
-        .catch(err=>{
-            console.log(err);
-        });
-        
-    }
-     
-    function showOutput()
-    {     
-        let users = Object.keys(localStorage)
-         users.forEach(user => {
-            let a = JSON.parse( localStorage.getItem( user))
-            addtoDom(a);
-        });
-    }
 
-      function addtoDom(a)
-      {
-        // console.log(a)
-        let liEle = document.createElement("li");
-        let btn = document.createElement("button");
-        let editbtn = document.createElement("button")
-        liEle.textContent =  a.name+"   " + a.email+"   "+a.phone;
-        btn.innerText = "delete"
-        btn.className = "delete btn btn-danger btn-sm ";
-        btn.addEventListener("click",deleteuser)
-        editbtn.textContent = "edit";
-        editbtn.className = "edit btn btn-success btn-sm"
-        editbtn.addEventListener("click" , edituser)
+// gets called when we add  a new user
+function dothis(e) {
+  e.preventDefault();
+  let user = e.target;
 
-        liEle.appendChild(btn)
-        liEle.appendChild(editbtn)
-        ulItems.appendChild(liEle);
-      }
-
-    
-    function deleteuser(e)
-    {   
-        let li = e.target.parentElement;
-        let ul = li.parentElement;
-        let key = li.textContent.split(" ")[0];
-         
-        localStorage.removeItem(key);
-
-        ulItems.removeChild(li);
-
-    }
-
-    let a = new Set();
+  let userobj = {
+    name: user.name.value,
+    email: user.email.value,
+    phone: user.phone.value,
+  };
+  axios
+  .post(
+    "https://crudcrud.com/api/eb7de077b62e4abab2890bff988fc248/users",
+    userobj
+    )
+    .then((res) => {
+      localStorage.setItem(res.data._id,JSON.stringify(res.data));
+      addtoDom(res.data._id);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 
-    function edituser(e)
-    {
-       let liele =  e.target.parentElement ;
-       let key = liele.textContent.split(" ")[0];
-       console.log(key)
-       let user = JSON.parse(localStorage.getItem(key))
-       localStorage.removeItem(key);
 
-       nameInp.value  = user.name;
-       emailInp.value = user.email;
-       phoneInp.value = user.number
+// function that first called to get the user on the screeen for the first time
+function LoadListItems(users) {
+  users.forEach((user) => {
+    localStorage.setItem(user._id , JSON.stringify(user))
+    addtoDom(user._id);
+  });
+}
 
-       ulItems.removeChild(liele)
 
-    }
+
+// function that adds a list ele to the dom;
+function addtoDom(a) {
+  let liEle = document.createElement("li");
+  let btn = document.createElement("button");
+  let editbtn = document.createElement("button");
+
+  let userDetails = JSON.parse( localStorage.getItem(a));
+  liEle.textContent =  userDetails.name 
+  + "   " +  userDetails.phone + "   " +
+  userDetails.email;
+
+  btn.innerText = "delete";
+  btn.className = "delete btn btn-danger btn-sm ";
+  btn.addEventListener("click", deleteuser);
+  editbtn.textContent = "edit";
+  editbtn.className = "edit btn btn-success btn-sm";
+  editbtn.addEventListener("click", edituser);
+
+  liEle.id = a;
+  liEle.appendChild(btn);
+  liEle.appendChild(editbtn);
+  ulItems.appendChild(liEle);
+}
+
+
+
+// function that deletes the user li item from the dom and userdata array
+function deleteuser(e) {
+  let li = e.target.parentElement;
+  let ul = li.parentElement;
+  let idTodelete = li.getAttribute("id");
+ 
+  ulItems.removeChild(li);
+  deleteFromStorage(idTodelete);
+}
+
+
+//function that actually deletes an item from the userdaa array
+function deleteFromStorage(userId) {
+  
+  localStorage.removeItem(userId);
+  axios.delete(`https://crudcrud.com/api/eb7de077b62e4abab2890bff988fc248/users/${userId}`)
+  .then(res => {
+    console.log(res)
+  })
+  .catch(err => {
+    console.error(err); 
+  })
+}
+
+
+// function to edit user
+function edituser(e) {
+  let liele = e.target.parentElement;
+  let idTodelete = liele.getAttribute("id");
+  let user = JSON.parse(localStorage.getItem(idTodelete))
+  deleteFromStorage(idTodelete);
+  nameInp.value = user.name;
+  emailInp.value = user.email;
+  phoneInp.value = user.phone;
+
+  ulItems.removeChild(liele);
+}
